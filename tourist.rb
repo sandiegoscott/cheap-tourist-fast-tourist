accd8da5553c25c697050f237e0578f2bca8e025
Scott Olmsted <solmsted@gmail.com>
2015-07-05 21:28:06 -0400

    Solves the minimum cost route

--- tourist.rb
+++ tourist.rb
@@ -15,19 +15,19 @@ end
 class Flight
   include Comparable
   attr_reader :origin, :destination, :arrival, :departure, :price
-  attr_accessor :cost, :start_time
+  attr_accessor :cost, :first_departure
 
   def initialize( flight_params )
     # flight info
     @origin, @destination, departs, arrives, currency = flight_params.split
-    @departure = departs.to_time
+    @departure = departs == 'nil' ? nil : departs.to_time
     @arrival = arrives.to_time
     price = currency.split('.')
     @price = 100 * price[0].to_i + price[1].to_i
 
     # info for route ending with this flight
     @cost = 0
-    @start_time = '00:00'.to_time
+    @first_departure = '00:00'.to_time
   end
 
   def <=>( another_flight )
@@ -39,9 +39,15 @@ class Flight
   end
 
   def inspect
-    depart = @departure.strftime("%H:%M")
-    arrive = @arrival.strftime("%H:%M")
+    depart = @departure ? @departure.strftime("%H:%M") : ' --- '
+    arrive = @arrival ? @arrival.strftime("%H:%M") : ' --- '
     "#{origin} #{@destination} #{depart} #{arrive} " + sprintf('%.2f', 0.01*@price) + ' ' + sprintf('%7.2f', 0.01*@cost)
+  end
+
+  def solution
+    arrive = @arrival.strftime("%H:%M")
+    start = @first_departure.strftime("%H:%M")
+    "#{start} #{arrive} " + ' ' + sprintf('%7.2f', 0.01*@cost)
   end
 end
 
@@ -52,13 +58,13 @@ class FlightQueue
   
   def dequeue
     flight = @flights.pop
-    puts "DEQUEUE #{flight.inspect}"
+    #puts "DEQUEUE #{flight.inspect}"
     flight
   end
   
   def enqueue(flight)
     @flights.unshift(flight)
-    puts "ENQUEUE #{flight.inspect}"
+    #puts "ENQUEUE #{flight.inspect}"
     self
   end
   
@@ -79,7 +85,7 @@ class BestFlights
   end
 
   def add( flight )
-    puts "ADDHEAP #{flight.inspect}"
+    #puts "ADDHEAP #{flight.inspect}"
     flights = @heap[flight.destination]
     if flights
       flights.select!{ |in_flight| in_flight.info != flight.info }  # remove this flight if present
@@ -95,7 +101,7 @@ class BestFlights
   end
 
   def inspect
-    'HEAP: ' + @heap.keys.map{|dest| dest + ': ' + @heap[dest].map{|flt| flt.inspect}.join('/')}.join('|')
+    'HEAP: ' + @heap.keys.map{|dest| dest + ': ' + @heap[dest].map{|flt| flt.inspect}.join('/')}.join(' | ')
   end
 end
 
@@ -103,13 +109,13 @@ def minimize_cost( q, arrivals )
   best_flights = BestFlights.new
 
   # start with a dummy flight in the heap of flights with best costs
-  best_flights.add Flight.new('- A 00:00, 00:00, 0.00')
+  best_flights.add Flight.new('- A 00:00 00:00, 0.00')
   arrivals['A'] = 1
 
-  puts q.inspect
-  puts best_flights.inspect
-  puts
-  puts 'START'
+  #puts q.inspect
+  #puts best_flights.inspect
+  #puts
+  #puts 'START'
 
   # while there are still flights in the queue--
   while q.size > 0
@@ -120,17 +126,19 @@ def minimize_cost( q, arrivals )
     in_flights = best_flights.arrivals(flight.origin)
     # if there are no incoming flights to add this flight to, put it at the end of the queue, we'll consider it again
     if in_flights.nil?
-      puts "No inflights"
+      #puts "No inflights"
       q.enqueue(flight)
       next
     end
     # find the lowest cost route that includes this flight
     lowest_cost = 100000000
+    #puts "Inflights to #{flight.origin}: #{in_flights.size}"
     in_flights.each do |in_flight|
       # if the flights are compatible and together are the lowest cost
       if in_flight.arrival <= flight.departure && in_flight.cost + flight.price < lowest_cost
         # save that information in the flight
         flight.cost = lowest_cost = in_flight.cost + flight.price
+        flight.first_departure = flight.origin == 'A' ? flight.departure : in_flight.first_departure
       end
     end
     # add this flight to the heap
@@ -138,13 +146,17 @@ def minimize_cost( q, arrivals )
     # if not all the incoming flights have been considered, put this flight in the queue to be considered again
     if best_flights.arrivals(flight.origin).size < arrivals[flight.origin]
       q.enqueue(flight)
+    else
+      #puts 'Not queued again'
     end
-    
-    puts "END OF LOOP"
-    puts q.inspect
-    puts best_flights.inspect
-    puts
   end
+
+  # retrieve best route
+  all_routes = best_flights.arrivals('Z')
+  #puts all_routes.inspect
+  best_route = all_routes.min_by { |flight| flight.cost }
+  puts best_route.solution
+  puts
 end
 
 #
@@ -172,9 +184,11 @@ while true
     line = file.gets.chomp
     # add to flight queue
     flight = Flight.new(line)
-    flights << flight
-    # count arrivals into each city
-    arrivals[flight.origin] = arrivals[flight.destination] ? arrivals[flight.destination] + 1 : 1
+    unless flight.origin == 'Z' || flight.destination == 'A'
+      flights << flight 
+      # count arrivals into each city
+      arrivals[flight.destination] = arrivals[flight.destination] ? arrivals[flight.destination] + 1 : 1
+    end
   end
 
   # queue the flights by arrival time
